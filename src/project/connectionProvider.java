23c81e2ebb0a2590dda72574b85f312b435f0b4a
Yash <babuy066@gmail.com>
2022-04-15 22:41:05 +0530

    first commit

---
+++ src/project/connectionProvider.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+import java.sql.*;
+/**
+ *
+ * @author HP PC
+ */
+public class connectionProvider {
+    public static Connection getCon()
+            
+    {
+     
+        try
+        {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/project","root","");
+        return con;
+        }
+        
+       catch(ClassNotFoundException | SQLException e)
+       {
+        return null;
+       }
+    }
+    
+}
